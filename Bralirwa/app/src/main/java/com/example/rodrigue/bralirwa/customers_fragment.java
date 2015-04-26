acc43f8223d82aa3c8a059aa4fbd9bd3bfe90041
Rodriguez917 <gsrrodriguez7@gmail.com>
2015-04-26 22:40:51 +0200

    changes

--- Bralirwa/app/src/main/java/com/example/rodrigue/bralirwa/customers_fragment.java
+++ Bralirwa/app/src/main/java/com/example/rodrigue/bralirwa/customers_fragment.java
@@ -2,6 +2,7 @@ package com.example.rodrigue.bralirwa;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,8 +10,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -48,5 +51,14 @@ public class customers_fragment extends android.support.v4.app.Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.products_list);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,int position, long arg3)
+            {
+                Intent launchOrder = new Intent(getActivity().getApplicationContext(), Order.class);
+                startActivity(launchOrder);
+            }
+        });
     }
 }
